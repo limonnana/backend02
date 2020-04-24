913b452022ac58aed3387e49991731b2fa2141f4
limonnana <rosenzvaig@gmail.com>
2020-04-24 14:12:45 +0300

    products

---
+++ src/main/java/com/limonnana/backend02/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.limonnana.backend02.controller;
+
+
+import com.google.gson.Gson;
+import com.limonnana.backend02.entity.Product;
+import com.limonnana.backend02.entity.User;
+import com.limonnana.backend02.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "**", maxAge = 3600)
+@RestController
+@RequestMapping("/secure/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(value="/findAll")
+    public List findAll(@RequestHeader Map<String, String> m) {
+
+        List<Product> l = productRepository.findAll();
+
+        return l;
+    }
+
+    @PostMapping(value="/create")
+    public Product create(@RequestBody Product product) {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(product));
+        return productRepository.save(product);
+    }
+
+}
---
+++ src/main/java/com/limonnana/backend02/entity/Product.java
@@ -0,0 +1,53 @@
+package com.limonnana.backend02.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String price;
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
